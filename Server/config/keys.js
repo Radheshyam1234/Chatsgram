3ad3c8541532256e7ff4e2c60fa591d093c75a1d
Radheshyam Kr <88738458+Radheshyam1234@users.noreply.github.com>
2021-12-20 09:24:42 +0530

    Delete Server/config directory

--- Server/config/keys.js
+++
@@ -1,5 +0,0 @@
-   module.exports={
-    MONGOURI : "mongodb+srv://Radheshyam:EI5N3f5xlYjZm3X3@cluster0.uexqn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET :"effegwegwrgrgw4"
-
-};
